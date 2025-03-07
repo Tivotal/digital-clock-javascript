7de2068f2293fbbc9442fcedf4d232de70c0f9b3
Tivotal <121335284+Tivotal@users.noreply.github.com>
2025-03-07 11:03:14 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+/* Created by Tivotal */
+
+let hrText = document.querySelector(".hour");
+let minText = document.querySelector(".min");
+let subText = document.querySelector(".sub");
+
+let dayText = document.querySelector(".day");
+let dateText = document.querySelector(".date");
+
+let icon = document.querySelector(".icon");
+
+setInterval(() => {
+  let date = new Date();
+
+  let hours = date.getHours();
+  let mins = date.getMinutes();
+
+  let label = hours < 12 ? "AM" : "PM";
+
+  hours = hours > 12 ? hours - 12 : hours;
+  hours = hours == 0 ? 12 : hours;
+  hours = hours < 10 ? "0" + hours : hours;
+
+  mins = mins < 10 ? "0" + mins : mins;
+
+  hrText.innerText = hours;
+  minText.innerText = mins;
+  subText.innerText = label;
+
+  dayText.innerText = date.toLocaleString("en", {
+    weekday: "long",
+  });
+
+  let fullDate =
+    date.getDate() +
+    " " +
+    date.toLocaleString("en", {
+      month: "long",
+    }) +
+    " " +
+    date.getFullYear();
+
+  dateText.innerText = fullDate;
+
+  if (label === "AM") {
+    icon.classList.replace("fa-moon", "fa-sun");
+  } else {
+    icon.classList.replace("fa-sun", "fa-moon");
+  }
+}, 1000);
